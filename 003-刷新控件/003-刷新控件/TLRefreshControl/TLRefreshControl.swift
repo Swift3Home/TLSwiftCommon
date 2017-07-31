631440fd12e26e53f609f6b76fae8c24dbd6dd83
lichuanjun <lichuanjun@niwodai.net>
2017-07-31 14:41:15 +0800

    移除 KVO 监听

--- 003-刷新控件/003-刷新控件/TLRefreshControl/TLRefreshControl.swift
+++ 003-刷新控件/003-刷新控件/TLRefreshControl/TLRefreshControl.swift
@@ -48,7 +48,22 @@ class TLRefreshControl: UIControl {
         scrollView?.addObserver(self, forKeyPath: "contentOffset", options: [], context: nil)
     }
     
+    // 本视图从父视图上移除
+    // 提示：所有的下拉刷新框架都是监听父视图的 contentOffset
+    // 所有的框架的 KVO 监听实现思路都是这个
+    override func removeFromSuperview() {
+        // 移除 KVO 监听
+        superview?.removeObserver(self, forKeyPath: "contentOffset")
+        
+        super.removeFromSuperview()
+        
+    }
+    
     // 所有 KVO 方法会统一调用此方法
+    // 在程序中，通常只监听某一个对象的某几个属性，如果属性太多，方法会很乱
+    // 观察者模式，在不需要时，都需要释放
+    // - 通知通知：如果不释放，什么也不会发生，但是会有内存泄露，会有多次注册的可能
+    // - KVO：如不释放，会崩溃
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         
         // contentOffset 的 y 值与 contentInset 的 top 有关
